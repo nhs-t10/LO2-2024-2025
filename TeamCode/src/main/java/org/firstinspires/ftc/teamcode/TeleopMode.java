9f1ccc7edcab17432108cb86a6f8e52078f4edc3
jdubster16 <154557581+jdubster16@users.noreply.github.com>
2024-09-12 15:37:29 -0400

    Update TeleopMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -1,3 +1,4 @@
+//robotics code that does things
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
@@ -9,36 +10,38 @@ import com.qualcomm.robotcore.hardware.Servo;
 
 import org.firstinspires.ftc.robotcore.internal.opmode.TelemetryImpl;
 
+
+//defines motors back left, back right,
 @TeleOp
 public class TeleopMode extends OpMode {
     private DcMotor frontLeft;
     private DcMotor frontRight;
     private DcMotor backRight;
     private DcMotor backLeft;
- 
+
+//puts motor names into phone language     
     @Override
     public void init() {
-        telemetry = new TelemetryImpl(this);
         frontLeft = hardwareMap.get(DcMotor.class, "fl");
         frontRight = hardwareMap.get(DcMotor.class, "fr");
         backLeft = hardwareMap.get(DcMotor.class, "bl");
         backRight = hardwareMap.get(DcMotor.class, "br");
     }
-
+//takes motor power and translates into joystick movements
     public void driveOmni(double y, double rx, double x){
         double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double flPower = (y + x + rx) / maxValue;
         double blPower = (y - x + rx) / maxValue;
         double frPower = (y - x - rx) / maxValue;
         double brPower = (y + x - rx) / maxValue;
-
+//sets motor power combined with joystick equal to phone language
         frontLeft.setPower(-flPower);
         frontRight.setPower(frPower);
         backLeft.setPower(-blPower);
         backRight.setPower(brPower);
     }
     
-    public void driveRaw(double flPower, double frPower, double brPower, double blPower){
+   /* public void driveRaw(double flPower, double frPower, double brPower, double blPower){
         //fl, fr, br, bl
         double[] motorCoefficients = {1,1,1,1};
         frontLeft.setPower(motorCoefficients[0]*flPower);
@@ -46,13 +49,13 @@ public class TeleopMode extends OpMode {
         backRight.setPower(motorCoefficients[2]*brPower);
         backLeft.setPower(motorCoefficients[3]*blPower);
     }
-   
+   */
 
 
-
+//takes entire thing and makes it work cause yeah :)
 }
     public void loop() {
             driveOmni(-1*gamepad1.left_stick_y, 1*gamepad1.right_stick_x, 1*gamepad1.left_stick_x);
 
     }
-}
+}    
