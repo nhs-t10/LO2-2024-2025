df9ff3f03fe47db0fa1abdb26fd23492634fd442
The-Ergster <Erg95@students.needham.k12.ma.us>
2024-12-10 20:32:11 -0500

    Revamped DragRaceAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragRaceAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DragRaceAuto.java
@@ -1,77 +1,39 @@
-// This code is from last year!
-
 package org.firstinspires.ftc.teamcode;
 
-import android.annotation.SuppressLint;
-
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import com.qualcomm.robotcore.eventloop.opmode.OpMode;
-import com.qualcomm.robotcore.hardware.ColorSensor;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.util.ElapsedTime;
-import com.qualcomm.robotcore.hardware.LightSensor;
-import com.qualcomm.robotcore.hardware.SwitchableLight;
-
-
-import vision.Webcam;
-// import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 @Autonomous
 public class DragRaceAuto extends OpMode {
-    //@Override
-    // This below here is the code for the actual claw
-// driveOmni(-1*gamepad1.left_stick_y, 1*gamepad1.right_stick_x, 1*gamepad1.left_stick_x);
+
     private DcMotor frontLeft;
     private DcMotor frontRight;
-    private DcMotor backRight;
     private DcMotor backLeft;
-    // down there makes a webcam
-//    private Webcam webcam;
-    ColorSensor colorSensor;
+    private DcMotor backRight;
+    private DcMotor linearSlideMotor;
+    private DcMotor armMotor;
 
-    int step=0;
-    int delayStep=-1;
-    double endTime;
-    public ElapsedTime timer = new ElapsedTime();
+    private ElapsedTime timer = new ElapsedTime();
 
-    public void delayedStop(double delay){
-        if (delayStep!=step){
-            delayStep=step;
-            endTime=timer.milliseconds()+delay;
-
-        }
-        if (timer.milliseconds()>=endTime) {
-            stopRobot();
-            step++;
-        }
-        //stops
-    }
-
+    @Override
     public void init() {
         frontLeft = hardwareMap.get(DcMotor.class, "fl");
         frontRight = hardwareMap.get(DcMotor.class, "fr");
         backLeft = hardwareMap.get(DcMotor.class, "bl");
         backRight = hardwareMap.get(DcMotor.class, "br");
-        colorSensor = hardwareMap.get(ColorSensor.class, "sensor_color");
+        linearSlideMotor = hardwareMap.get(DcMotor.class, "linearSlide");
+        armMotor = hardwareMap.get(DcMotor.class, "arm");
+
+        // Reset motors to ensure proper operation
+        linearSlideMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+
+        linearSlideMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
+        armMotor.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
     }
 
-    boolean getBrightness() {
-        //telemetry.addLine(colors.alpha);
-        float blue = (float) (colorSensor.blue() * 3 - colorSensor.green() - (1.5 * colorSensor.red()));
-        telemetry.addLine()
-                .addData("Red", "%d", colorSensor.red())
-                .addData("Green", "%d", colorSensor.green())
-                .addData("Blue", "%d", colorSensor.blue());
-        telemetry.addLine(String.valueOf(blue));
-        telemetry.update();
-        if (colorSensor.blue() > 150 && colorSensor.blue() < 175 && colorSensor.red() < 85 && colorSensor.red() > 50 && colorSensor.green() > 100 && colorSensor.green() < 150) {
-            return true;
-        } else {
-            //return blue;
-            return false;
-        }
-    }
-
+    // Omni drive function
     public void driveOmni(double y, double rx, double x) {
         double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double flPower = (y + x + rx) / maxValue;
@@ -83,10 +45,10 @@ public class DragRaceAuto extends OpMode {
         frontRight.setPower(frPower);
         backLeft.setPower(-blPower);
         backRight.setPower(brPower);
-
     }
 
-    public void stopRobot(){
+    // Function to stop the robot
+    public void stopRobot() {
         frontLeft.setPower(0);
         frontRight.setPower(0);
         backLeft.setPower(0);
@@ -95,10 +57,78 @@ public class DragRaceAuto extends OpMode {
 
     @Override
     public void loop() {
-//        colorSensor.setGain(15);
-        colorBlue = getBrightness();
-        driveOmni(0.35, 00, 0);
- //*/
+
+        switch (step) {
+            case 0:
+                // Drive left for 5 feet (assuming 1 ft = 12 inches, 5 feet = 60 inches)
+                // Adjust speed and duration based on your robot's drive system.
+                driveOmni(0, 0, -0.5);  // Negative X for leftward motion
+                timer.reset();
+                step++;
+                break;
+
+            case 1:
+                // Wait until the robot has driven for 5 feet.
+                if (timer.seconds() >= 2) {  // Adjust this duration based on testing
+                    stopRobot();
+                    step++;
+                }
+                break;
+
+            case 2:
+                // Turn 45 degrees to the right
+                driveOmni(0, 0.35, 0);  // Rotate by positive rx (turn to the right)
+                timer.reset();
+                step++;
+                break;
+
+            case 3:
+                // Wait for the 45-degree turn to complete
+                if (timer.seconds() >= 1) {  // Adjust based on robot turn speed
+                    stopRobot();
+                    step++;
+                }
+                break;
+
+            case 4:
+                // Raise the linear slide (assuming a motor-based linear slide)
+                linearSlideMotor.setPower(1.0);  // Raise the slide
+                timer.reset();
+                step++;
+                break;
+
+            case 5:
+                // Wait for the linear slide to fully raise (adjust duration based on slide speed)
+                if (timer.seconds() >= 2) {
+                    linearSlideMotor.setPower(0);  // Stop the linear slide motor
+                    step++;
+                }
+                break;
+
+            case 6:
+                // Move the arm fully to the other side (assuming the arm motor is for this)
+                armMotor.setPower(1.0);  // Move the arm
+                timer.reset();
+                step++;
+                break;
+
+            case 7:
+                // Wait for the arm to reach the other side (adjust duration based on arm speed)
+                if (timer.seconds() >= 2) {
+                    armMotor.setPower(0);  // Stop the arm motor
+                    step++;
+                }
+                break;
+
+            case 8:
+                // End of autonomous, stop everything
+                stopRobot();
+                break;
+        }
+
+        telemetry.addData("Step", step);
+        telemetry.update();
     }
 
+    private int step = 0;
 }
