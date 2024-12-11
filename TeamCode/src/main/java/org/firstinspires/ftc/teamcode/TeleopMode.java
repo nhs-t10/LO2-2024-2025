53c80f6ecf72b52adec7085675bfc871f4d8969e
The-Ergster <Erg95@students.needham.k12.ma.us>
2024-12-10 20:26:35 -0500

    Remade TeleopMode.java better! take that hardware !!!!1

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TeleopMode.java
@@ -1,56 +1,21 @@
-//robotics code that does things
 package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.CRServo;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
-import com.qualcomm.robotcore.hardware.Servo;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
-import org.firstinspires.ftc.robotcore.internal.opmode.TelemetryImpl;
-
-//Boo
-// defines motors back left, back right,
 @TeleOp
 public class TeleopMode extends OpMode {
-    private DcMotor frontLeft;
-    private DcMotor frontRight;
-    private DcMotor backRight;
-    private DcMotor backLeft;
-    private DcMotor intake;
-    private DcMotor arm; // rotating arm with intake
-    private DcMotor bucket;
-    private DcMotor slides;
 
-    double armPower = 0.2; // this is a test number
-    double bucketPower = 0.1; // this is also a test number
-    double intakePower = 0.1; // this is also a test number
-    double slidePower = 0.2;
+    // Declare motors and other hardware components
+    private DcMotor frontLeft, frontRight, backLeft, backRight, intake, arm, bucket, slides;
+    private double armPower = 0.2, bucketPower = 0.1, intakePower = 0.1, slidePower = 0.2;
+    private int step = 0, delayStep = -1;
+    private double endTime;
+    private ElapsedTime timer = new ElapsedTime();
 
-//I like cheese
-    int step = 0;
-    int delayStep = -1;
-    double endTime;
-    public ElapsedTime timer = new ElapsedTime();
-
-    public void delayedStop(double delay){
-        if (delayStep != step){
-            delayStep = step;
-            endTime = timer.milliseconds()+delay;
-
-        }
-        if (timer.milliseconds()>=endTime) {
-            step++;
-        }
-        //stops
-    }
-
-
-//I eat cheese
-
-    //puts motor names into phone language
+    // Initialization of hardware
     @Override
     public void init() {
         frontLeft = hardwareMap.get(DcMotor.class, "fl");
@@ -61,154 +26,91 @@ public class TeleopMode extends OpMode {
         arm = hardwareMap.get(DcMotor.class, "ar");
         bucket = hardwareMap.get(DcMotor.class, "bu");
         slides = hardwareMap.get(DcMotor.class, "sl");
-
     }
-//takes motor power and translates into joystick movements
-    public void driveOmni(double y, double rx, double x){
+
+    // Omni drive logic
+    public void driveOmni(double y, double rx, double x) {
         double maxValue = Math.max(Math.abs(y) + Math.abs(x) + Math.abs(rx), 1);
         double flPower = (y + x + rx) / maxValue;
         double blPower = (y - x + rx) / maxValue;
         double frPower = (y - x - rx) / maxValue;
         double brPower = (y + x - rx) / maxValue;
 
-
-//sets motor power combined with joystick equal to phone language
-
         frontLeft.setPower(-flPower);
         frontRight.setPower(frPower);
         backLeft.setPower(-blPower);
         backRight.setPower(brPower);
-
-
-    }
-    
-   /* public void driveRaw(double flPower, double frPower, double brPower, double blPower){
-        //fl, fr, br, bl
-        double[] motorCoefficients = {1,1,1,1};
-        frontLeft.setPower(motorCoefficients[0]*flPower);
-        frontRight.setPower(motorCoefficients[1]*frPower);
-        backRight.setPower(motorCoefficients[2]*brPower);
-        backLeft.setPower(motorCoefficients[3]*blPower);
-    }
-   */
- /*   public void flipArm() {
-
-        arm.setPower(armPower);
-         //if(){ //slidesDown will be a boolean about slide position, we will make it later
-            step = 1;
-         // }
-
-        switch(step) {
-            case(0):
-                // make sure slide bucket is lowered [only runs if slides need to move down]
-                slides.setPower(-.3);
-                delayedStop(1000);
-                // set slidesDown to true
-                break;
-            case(1):
-                // move arm to back
-                arm.setPower(-1 * armPower);
-                delayedStop(1000);
-                break;
-            case(2):
-                // drop sample
-                intake.setPower(-1 * intakePower);
-                delayedStop(1000);
-                break;
-            case(3):
-                // flip arm back
-                arm.setPower(armPower);
-                delayedStop(1000);
-                break;
-
-        }
-
-    } */
-
-    public void backwardsFlipArm() {
-        arm.setPower(armPower);
-         //if(){ //slidesDown will be a boolean about slide position, we will make it later
-            step = 1;
-         // }
-
-        switch(step) {
-            case(0):
-                // make sure slide bucket is lowered [only runs if slides need to move down]
-                slides.setPower(-.3);
-                delayedStop(1000);
-                // set slidesDown to true
-                break;
-            case(1):
-                // move arm to back
-                slides.setPower(0);
-                arm.setPower(-1 * armPower);
-                delayedStop(1000);
-                break;
-            case(2):
-                // drop sample
-                arm.setPower(0);
-                intake.setPower(-1 * intakePower);
-                delayedStop(1000);
-                break;
-            case(3):
-                intake.setPower(0);
-
-        }
-
     }
 
-
-
-    public void forwardsFlipArm() {
-
-        arm.setPower(armPower);
-         //if(){ //slidesDown will be a boolean about slide position, we will make it later
-            step = 1;
-         // }
-
-        switch(step) {
-            case(0):
-                // flip arm back
-                arm.setPower(armPower);
-                delayedStop(1000);
-                break;
-            case(1):
-                arm.setPower(0);
-                break;
-        }
-
-    }
-
-
-
-// takes entire thing and makes it work cause yeah :)
-
-    public void loop() {
-        driveOmni(-1*gamepad1.left_stick_y, 1*gamepad1.right_stick_x, 1*gamepad1.left_stick_x);
-
-        if (gamepad1.a){
-            intake.setPower(.15);
+    // Control Intake motor
+    private void controlIntake() {
+        if (gamepad1.a) {
+            intake.setPower(0.15); // Intake in
         } else if (gamepad1.b) {
-            intake.setPower(-.15);
+            intake.setPower(-0.15); // Intake out
         } else {
-            intake.setPower(0);
+            intake.setPower(0); // Stop intake
         }
-
-
-        if (gamepad1.x){
-            backwardsFlipArm();
-        } else if (gamepad1.y){
-            forwardsFlipArm();
-        }
-
-
-        if (gamepad1.dpad_up) {
-            slides.setPower(slidePower);
-        } else if (gamepad1.dpad_down) {
-            slides.setPower(-slidePower);
-        }
-
-
     }
 
-}    
+    // Control Arm motion
+    private void controlArm() {
+        if (gamepad1.x) {
+            flipArm(true); // Flip arm backwards
+        } else if (gamepad1.y) {
+            flipArm(false); // Flip arm forwards
+        }
+    }
+
+    // Flip Arm logic (Backwards or Forwards)
+    private void flipArm(boolean isBackwards) {
+        arm.setPower(isBackwards ? -armPower : armPower);
+
+        // Set delay logic if necessary
+        delayedStop(1000);
+    }
+
+    // Slide control
+    private void controlSlides() {
+        if (gamepad1.dpad_up) {
+            slides.setPower(slidePower); // Move slides up
+        } else if (gamepad1.dpad_down) {
+            slides.setPower(-slidePower); // Move slides down
+        } else {
+            slides.setPower(0); // Stop slides
+        }
+    }
+
+    // Delayed stop logic (general purpose)
+    public void delayedStop(double delay) {
+        if (delayStep != step) {
+            delayStep = step;
+            endTime = timer.milliseconds() + delay;
+        }
+        if (timer.milliseconds() >= endTime) {
+            step++; // Move to next step after delay
+        }
+    }
+
+    // Main loop for controlling robot
+    @Override
+    public void loop() {
+        // Drive control
+        driveOmni(-gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_stick_x);
+
+        // Intake control
+        controlIntake();
+
+        // Arm control (forwards/backwards flip)
+        controlArm();
+
+        // Slide control (up/down)
+        controlSlides();
+
+        // Telemetry for debugging
+        telemetry.addData("Arm Power", arm.getPower());
+        telemetry.addData("Intake Power", intake.getPower());
+        telemetry.addData("Slide Power", slides.getPower());
+        telemetry.update();
+    }
+}
